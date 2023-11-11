e2e94711b6882e374598096cb68bc392a0de06f1
Your Name <weolopez@gmail.com>
2023-11-11 08:31:41 -0500

    board

--- src/app/games/risk/board/board.component.ts
+++ src/app/games/risk/board/board.component.ts
@@ -22,11 +22,12 @@ export class RiskBoardComponent {
 
   territories: Array<any> = territories
   players: Array<any> = [
-    { name: 'John', color: '#FF5733' },
-    { name: 'Emily', color: '#33FFC1' },
-    { name: 'David', color: '#FF33E9' },
-    { name: 'Sarah', color: '#33FF57' },
-    { name: 'Michael', color: '#FF5733' },
+    { name: 'John', color: 'lightblue', armies: 50 },
+    { name: 'Emily', color: '#33FFC1', armies: 50 },
+    { name: 'David', color: '#FF33E9', armies: 50 },
+    { name: 'Sarah', color: '#33FF57', armies: 50 },
+    { name: 'Michael', color: 'green', armies: 50 },
+    { name: 'Tom', color: 'pink', armies: 50 },
   ];
   features!: { type: string; geometry: { type: string; coordinates: number[]; }; properties: { name: string; address: string; description: string; image: string; website: string; phone: string; }; }[];
 phase: any="init";
@@ -38,16 +39,88 @@ phase: any="init";
     //make a randome player go first
     this.currentPlayer = Math.floor(Math.random() * this.players.length);
   }
-  assign() {
-    let p=0
-    console.log(this.territories.length)
-    this.territories = this.territories.map(t => {
-      t.owner = this.currentPlayer
-      this.next()
-      // p = p+1
-      // if (p>=this.players.length) p = 0;
-      return t
-    })
+  auto() {
+// switch statment for the different phases of risk
+    switch (this.phase) {
+      case 'init':
+        this.claim()
+        this.phase = 'place'
+        break;
+      case 'place':
+        this.place()
+        this.phase = 'play'
+        break;
+      case 'play':
+        this.play()
+        this.phase = 'fortify'
+        break;
+      // case 'fortify':
+      //   this.fortify()
+      //   this.phase = 'place'
+      //   break;
+    }
+  }
+  place() {
+    let all_armies_placed = false
+    while (all_armies_placed == false) {
+      this.players.forEach((p, index) => {
+        // randomly select a territory that is owned by the player and place an army on it
+        let available_territories = this.territories.filter(t => {
+          console.log(t.owner)
+        return  t.owner == index
+        })
+        if (available_territories.length > 0 && this.players[index].armies > 0) {
+          let territory = available_territories[Math.floor(Math.random() * available_territories.length)]
+          if (!territory.armies) territory.armies = 0
+          territory.armies = territory.armies + 1
+          this.players[index].armies = this.players[index].armies - 1
+        } else {
+          all_armies_placed = this.players.every(p => p.armies <= 0)
+        }
+      })
+    }
+  }
+  // attack() {
+
+  claim() {
+        // loop until all territories are owned
+        let territories_all_owned = false
+        while (!territories_all_owned) {
+          this.players.forEach((p, index) => {
+            // randomly select a territory that is not owned and assign it to the player
+            let available_territories = this.territories.filter(t => (!t.owner && t.owner !== 0) )
+            if (available_territories.length >  0) {
+            let territory = available_territories[Math.floor(Math.random() * available_territories.length)]
+            territory.owner = index
+            } else {
+              territories_all_owned = true
+            }
+          })
+        }
+  }
+  play() {
+    // this is the main game loop, every round each player gets a turn, and can attack, fortify, or end their turn
+    let game_over = false
+    while (!game_over) {
+      this.players.forEach((p, index) => {  
+        //give player armys based on the number of territories they own
+        p.armies = this.getTerritoriesByOwner(index).length / 3
+        // randomly select a territory that is owned by the player and place an army on it
+        
+      })
+    }
+  }
+ 
+
+getTerritoriesByOwner(player: any): Array<any> {
+  return this.territories.filter(territory => territory.owner === player);
+}
+
+  rollDice(numDice: number) {
+    throw new Error('Method not implemented.');
+  }
+  resolveBattle(attackRolls: any, defendRolls: any): string {
+    throw new Error('Method not implemented.');
   }
   shuffle(anyArray: Array<any>) {
     return  "any"
@@ -68,7 +141,9 @@ phase: any="init";
         this.phase = 'place'
         console.log(finished)
       }
-      else console.log(finished)
+      else {
+        console.log('next')
+      }
     }
     this.currentPlayer = this.currentPlayer + 1
     if (this.currentPlayer >= this.players.length) this.currentPlayer=0
